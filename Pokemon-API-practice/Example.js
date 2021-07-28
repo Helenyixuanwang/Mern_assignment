63627dcd2af73201781fc329cacb72b0bb72f08a
Helen WANG <wangyixuan@msn.com>
2021-07-28 10:20:15 -0700

    pokemon-api-pratice

---
+++ Pokemon-API-practice/Example.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect }from 'react';
+
+const Example = (props) => {
+    const [state, setState ] = useState([]);
+
+    useEffect(()=> {
+        console.log("Hello");
+        // fetch('https://swapi.dev/api/people')
+        // fetch('https://pokeapi.co/api/v2/pokemon')
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=800')
+        .then(response => {
+            return response.json()
+        })
+        .then(response => {
+            setState(
+                response.results
+            )
+        })
+        .catch((err) => {
+            console.log(err);
+          })
+            
+    },[]);
+    return (  
+        
+        <ol>
+            {state? state.map((item,index) =>{
+                return <li key={index}> {item.name} </li>
+            }):null}
+        </ol>
+        
+    );
+}
+ 
+export default Example;
