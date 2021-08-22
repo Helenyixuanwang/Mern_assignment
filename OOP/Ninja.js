9025355b4155bc7236040fd96fbdbdbb6e6e66d4
Helen WANG <wangyixuan@msn.com>
2021-08-22 15:50:53 -0700

    update Ninja.js

--- OOP/Ninja.js
+++ OOP/Ninja.js
@@ -12,11 +12,11 @@
 // Create a Ninja class  add an attribute: name  add an attribute: health  add a attribute: speed - give a default value of 3  add a attribute: strength - give a default value of 3  add a method: sayName() - This should log that Ninja's name to the console  add a method: showStats() - This should show the Ninja's name, strength, speed, and health.  add a method: drinkSake() - This should add +10 Health to the Ninja
 
 class Ninja {
-    constructor(name, health) {
+    constructor(name, health, speed=3,strength=3) {
         this.name = name;
         this.health = health;
-        this.speed = 3;
-        this.strength = 3;
+        this.speed = speed;
+        this.strength = strength;
     }
 
     sayName() {
@@ -29,10 +29,15 @@ class Ninja {
                 Health: ${this.health}`)
     }
     drinkSake() {
+        console.log("Drinking Sake now...")
         this.health += 10;
+        console.log(`Current strenght is ${this.health} after drinking sake.`)
     }
 }
+
+
 
 const ninja1 = new Ninja("Hyabusa", 100);
 ninja1.sayName();
 ninja1.showStats();
+ninja1.drinkSake();
